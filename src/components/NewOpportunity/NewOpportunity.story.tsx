1ba52552d96e887989b94827a041974ac2bf0ce0
Matt Kane <matt.kane@aerian.com>
2019-05-08 16:41:26 +0100

    Fix tests and remove tests and stories for graphql components

--- src/components/NewOpportunity/NewOpportunity.story.tsx
+++ src/components/NewOpportunity/NewOpportunity.story.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 import { NewOpportunity } from "./index";
 
 storiesOf("Components|NewOpportunity", module).add("Default", () => (
-    <NewOpportunity addOpportunity={() => {}} />
+    <NewOpportunity addOpportunity={action("New opportunity")} />
 ));
