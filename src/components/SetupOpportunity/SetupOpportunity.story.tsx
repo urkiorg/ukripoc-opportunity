2b15267d4a6c56fe00695bf7284a3db0b1bc9b05
Darren Torchia <darren.torchia@aerian.com>
2019-05-14 13:38:31 +0100

    update mock return

--- src/components/SetupOpportunity/SetupOpportunity.story.tsx
+++ src/components/SetupOpportunity/SetupOpportunity.story.tsx
@@ -13,15 +13,19 @@ const opportunity: GetOpportunityQuery = {
             nextToken: null,
             __typename: "ModelFunderConnection"
         },
+        fundersComplete: null,
         teammembers: {
             items: [],
             nextToken: null,
             __typename: "ModelTeamMemberConnection"
         },
+        teammembersComplete: null,
         type: null,
+        typeComplete: null,
         __typename: "Opportunity"
     }
 };
+
 storiesOf("Components|SetupOpportunity", module).add("Default", () => (
     <SetupOpportunity opportunity={opportunity} />
 ));
