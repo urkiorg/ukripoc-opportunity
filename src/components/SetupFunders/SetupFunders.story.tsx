84d7732ffbe3d8de399d27ed110ce5bd6bb1a9a9
Darren Torchia <darren.torchia@aerian.com>
2019-05-14 14:35:16 +0100

    revert my attempt at being tidy

--- src/components/SetupFunders/SetupFunders.story.tsx
+++ src/components/SetupFunders/SetupFunders.story.tsx
@@ -3,9 +3,29 @@ import { storiesOf } from "@storybook/react";
 import { SetupFunders } from "./index";
 import fundersList from "../../fixtures/funders.json";
 import { GetOpportunityQuery } from "../../API";
-import { getOpportunityQuery } from "../../fixtures/getOpportunityQuery.json";
 
-const opportunity: GetOpportunityQuery = getOpportunityQuery;
+const opportunity: GetOpportunityQuery = {
+    getOpportunity: {
+        id: "675c0700-09c3-4b1c-9292-71f96ef0567e",
+        name: "New opp",
+        description: "Today",
+        funders: {
+            items: [],
+            nextToken: null,
+            __typename: "ModelFunderConnection"
+        },
+        fundersComplete: null,
+        teammembers: {
+            items: [],
+            nextToken: null,
+            __typename: "ModelTeamMemberConnection"
+        },
+        teammembersComplete: null,
+        type: null,
+        typeComplete: null,
+        __typename: "Opportunity"
+    }
+};
 
 storiesOf("Components|SetupFunders", module).add("Default", () => (
     <SetupFunders funders={fundersList} opportunity={opportunity} />
