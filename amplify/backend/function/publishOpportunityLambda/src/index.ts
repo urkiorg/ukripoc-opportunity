4c60e52906f49d2c877cba5be0c66fe8a9e7f041
Sunya Dickman <sunya.dickman@aerian.com>
2019-05-17 16:33:15 +0100

    opportunity-publish: delete old lambda

--- amplify/backend/function/publishOpportunityLambda/src/index.ts
+++
@@ -1,9 +0,0 @@
-const awsServerlessExpress = require("aws-serverless-express");
-const publishOpportunityAPI = require("./app");
-
-const server = awsServerlessExpress.createServer(publishOpportunityAPI);
-
-exports.handler = (event, context) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    awsServerlessExpress.proxy(server, event, context);
-};
