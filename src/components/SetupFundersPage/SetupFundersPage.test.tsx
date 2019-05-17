00f6b498da2e3c5dee2d20a5f09846fc230180b9
Darren Torchia <darren.torchia@aerian.com>
2019-05-17 11:03:13 +0100

    remove test for page new snapshots

--- src/components/SetupFundersPage/SetupFundersPage.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { SetupFundersPage } from "./index";
-
-describe("SetupFundersPage", () =>
-    it("renders correctly", () => {
-        const tree = renderer.create(<SetupFundersPage />).toJSON();
-        expect(tree).toMatchSnapshot();
-    }));
