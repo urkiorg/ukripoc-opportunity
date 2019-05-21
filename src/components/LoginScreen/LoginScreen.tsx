587156447a284f987a739323452f56b885b88a57
Darren Ivey <darren.ivey@aerian.com>
2019-05-21 13:26:27 +0100

    Add error state

--- src/components/LoginScreen/LoginScreen.tsx
+++ src/components/LoginScreen/LoginScreen.tsx
@@ -5,6 +5,7 @@ import Input from "@govuk-react/input";
 import Checkbox from '@govuk-react/checkbox';
 import Button from '@govuk-react/button';
 import LabelText from '@govuk-react/label-text';
+import ErrorSummary from '@govuk-react/error-summary';
 import styles from "./LoginScreen.module.scss";
 import { Auth } from "aws-amplify";
 import { navigate } from "@reach/router";
@@ -18,6 +19,7 @@ export const LoginScreen: FC<Props> = (props) => {
     const [password, setPassword] = useState("");
     const [username, setUsername] = useState("");
     const [persistedLogin, setPersistedLogin] = useState(false);
+    const [error, setError] = useState(false);
     const [loading, setLoading] = useState(false);
 
     const onInputChangeUsername = (e: any) => {
@@ -39,22 +41,29 @@ export const LoginScreen: FC<Props> = (props) => {
         try {
             await Auth.signIn(username, password);
             setLoading(false);
+            setError(false)
             navigate(`/`)
         } catch(error) {
-            console.log("Error!", error)
+            setError(true)
             setLoading(false);
         }
     }
 
     return (
         <div>
+            { error &&
+                <ErrorSummary
+                    heading={"Your sign in was unsuccessful because of the following issues"}
+                    description={"Your email/password combination doesn't seem to work."}
+                    />
+            }
             <H3>Welcome</H3>
             <H2 textColour={ukriGreen}>Please log in</H2>
             <form onSubmit={handleSubmit}>
                 <LabelText>Username</LabelText>
-                <Input className={styles.input} value={username} onChange={onInputChangeUsername} />
+                <Input  error={error} className={styles.input} value={username} onChange={onInputChangeUsername} />
                 <LabelText>Password</LabelText>
-                <Input type="password" className={styles.input} value={password} onChange={onInputChangePassword} />
+                <Input  error={error} type="password" className={styles.input} value={password} onChange={onInputChangePassword} />
                 <Checkbox onChange={togglePersistedLogin}>Keep me logged in</Checkbox>
                 <Button type="submit" buttonColour={ukriGreen}>Login</Button>
             </form>
