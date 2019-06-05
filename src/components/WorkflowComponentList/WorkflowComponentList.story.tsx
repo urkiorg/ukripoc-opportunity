2466c351ef471ea3246320ec0dd50cfc6afd871d
Darren Ivey <darren.ivey@aerian.com>
2019-06-05 09:39:19 +0100

    Update test

--- src/components/WorkflowComponentList/WorkflowComponentList.story.tsx
+++ src/components/WorkflowComponentList/WorkflowComponentList.story.tsx
@@ -2,6 +2,22 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { WorkflowComponentList } from "./index";
 
+const websiteListings = [{
+    id: "1",
+    rank: 0,
+    lastPublished: "",
+    description: "Test webs site listing",
+}];
+
+const applications = [{
+    id: "1",
+    rank: 0,
+    openApplication: "",
+    closeApplication: "",
+}];
+
 // storiesOf("Components|WorkflowComponentList", module).add("Default", () => (
-//     <WorkflowComponentList websiteListings={[]} applications={[]} />
+//     <WorkflowComponentList 
+//         websiteListings={websiteListings} 
+//         applications={applications} />
 // ));
